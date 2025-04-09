5c43507981f766e02a60cd51ab54d9423c5b7371
mai1309 <atismailisa@gmail.com>
2025-04-09 15:36:39 -0500

    command line started

--- main.c
+++ main.c
@@ -17,20 +17,22 @@ int main() {
             printf("\nExiting...\n");
             break;
         }
-    }
-    
-    // Removes trailing newline so input can read correctly
-    input[strcspn(input, "\n")] = 0;
+        else
+        {
+            // Removes trailing newline so input can read correctly
+            input[strcspn(input, "\n")] = 0;
 
-    if (strcmp(input, "exit") == 0) {
-        exit(0);
-    } else if (strcmp(input, "help") == 0) {
-        printf("Available commands:\n");
-        printf(" - help\n");
-        printf(" - exit\n");
-        printf(" - (Add your own commands here...)\n");
-    } else {
-        printf("Unknown command: %s\n", input);
+            if (strcmp(input, "exit") == 0) {
+             exit(0);
+            } else if (strcmp(input, "help") == 0) {
+                printf("Available commands:\n");
+                printf(" - help\n");
+                printf(" - exit\n");
+                printf(" - (Add more commands here...)\n");
+            } else {
+                printf("Unknown command: %s\n", input);
+            }
+        }
     }
     return 0;
 }
